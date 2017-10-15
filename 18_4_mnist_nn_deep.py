3872152ed72fc311340dec4dd58e2316bc49a24c
jianmyphy123 <jianmyphy123@tutanota.com>
2017-10-15 15:53:22 +1100

    mnist nn deep

---
+++ 18_4_mnist_nn_deep.py
@@ -0,0 +1,90 @@
+# Lab 7 Learning rate and Evaluation
+import tensorflow as tf
+import random
+# import matplotlib.pyplot as plt
+from tensorflow.examples.tutorials.mnist import input_data
+tf.set_random_seed(777)  # reproducibility
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+# Check out https://www.tensorflow.org/get_started/mnist/beginners for
+# more information about the mnist dataset
+
+# parameters
+learning_rate = 0.001
+training_epochs = 15
+batch_size = 100
+
+# input place holders
+X = tf.placeholder(tf.float32, [None, 784])
+Y = tf.placeholder(tf.float32, [None, 10])
+
+# weights & bias for nn layers
+W1 = tf.get_variable('W1', shape=[784,512], initializer=tf.contrib.layers.xavier_initializer())
+b1 = tf.Variable(tf.random_normal([512]))
+L1 = tf.nn.relu(tf.matmul(X, W1) + b1)
+
+W2 = tf.get_variable('W2', shape=[512,512], initializer=tf.contrib.layers.xavier_initializer())
+b2 = tf.Variable(tf.random_normal([512]))
+L2 = tf.nn.relu(tf.matmul(L1, W2) + b2)
+
+W3 = tf.get_variable('W3', shape=[512,512], initializer=tf.contrib.layers.xavier_initializer())
+b3 = tf.Variable(tf.random_normal([512]))
+L3 = tf.nn.relu(tf.matmul(L2, W3) + b3)
+
+W4 = tf.get_variable('W4', shape=[512,512], initializer=tf.contrib.layers.xavier_initializer())
+b4 = tf.Variable(tf.random_normal([512]))
+L4 = tf.nn.relu(tf.matmul(L3, W4) + b4)
+
+W5 = tf.get_variable('W5', shape=[512,10], initializer=tf.contrib.layers.xavier_initializer())
+b5 = tf.Variable(tf.random_normal([10]))
+hypothesis = tf.matmul(L4, W5) + b5
+
+# define cost/loss & optimizer
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=hypothesis, labels=Y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+# initialize
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+# train my model
+for epoch in range(training_epochs):
+    avg_cost = 0
+    total_batch = int(mnist.train.num_examples / batch_size)
+
+    for i in range(total_batch):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        feed_dict = { X: batch_xs, Y: batch_ys }
+        c, _ = sess.run([cost, optimizer], feed_dict=feed_dict)
+        avg_cost += c / total_batch
+
+    print('Epoch:', '%04d' % (epoch+1), 'cost =', '{:.09f}'.format(avg_cost))
+
+print('Learning Finished')
+
+# Test model and check accuracy
+correct_prediction = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print('Accuracy: ', sess.run(accuracy, feed_dict={ X: mnist.test.images, Y: mnist.test.labels }))
+
+# Extracting MNIST_data/train-images-idx3-ubyte.gz
+# Extracting MNIST_data/train-labels-idx1-ubyte.gz
+# Extracting MNIST_data/t10k-images-idx3-ubyte.gz
+# Extracting MNIST_data/t10k-labels-idx1-ubyte.gz
+# Epoch: 0001 cost = 0.296610651
+# Epoch: 0002 cost = 0.105632383
+# Epoch: 0003 cost = 0.072340605
+# Epoch: 0004 cost = 0.052578560
+# Epoch: 0005 cost = 0.041032621
+# Epoch: 0006 cost = 0.035079713
+# Epoch: 0007 cost = 0.029851058
+# Epoch: 0008 cost = 0.025710791
+# Epoch: 0009 cost = 0.024499747
+# Epoch: 0010 cost = 0.020418911
+# Epoch: 0011 cost = 0.020451611
+# Epoch: 0012 cost = 0.018390832
+# Epoch: 0013 cost = 0.017786165
+# Epoch: 0014 cost = 0.015110528
+# Epoch: 0015 cost = 0.014775547
+# Learning Finished
+# Accuracy:  0.9784
